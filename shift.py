59c73c41469bb9a00b1f8904a614789c20f9b464
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-22 12:25:11 +0200

    1 commit

--- shift.py
+++ shift.py
@@ -1,6 +1,16 @@
+"""docstring"""
 from typing import List
 
 
-def shift(l: List[int], num: int) -> List[int]:
+def shift(input_list: List[int], num: int) -> List[int]:
+    """docstring"""
 
-    return l
+    result = []
+    strlen = len(input_list)
+    if strlen < num:
+        raise ValueError
+    for i in range(strlen):
+        j = (strlen - num + i - 1) % strlen
+        result.append(input_list[j])
+
+    return result
