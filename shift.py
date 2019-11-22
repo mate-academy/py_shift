68c141b9a10bebd200d43e52f495ea72379a96bc
vlysenok <tina.lysenok@gmail.com>
2019-11-22 13:03:50 +0200

    hw5_py_shift

--- shift.py
+++ shift.py
@@ -1,6 +1,19 @@
+"""
+shift module
+"""
 from typing import List
 
 
-def shift(l: List[int], num: int) -> List[int]:
-
-    return l
+def shift(lst: List[int], num: int) -> List[int]:
+    """
+    Perform a circular shift of a list to the left by a given number of elements.
+    :param lst: List[int]
+    :param num: int
+    :return: List[int]
+    """
+    if not lst:
+        return lst
+    if num > len(lst):
+        raise ValueError
+    num = num % len(lst)
+    return lst[num:] + lst[:num]
