5a95c59b44ca4a93ba459d45b56ff421e7243040
logotip <logotip123@yahoo.com>
2019-11-22 10:43:58 +0200

    v 1.0 logotip1992@gmail.com

--- shift.py
+++ shift.py
@@ -1,6 +1,11 @@
+"""module to perform a circular shift of a list to the left by a given number of elements."""
 from typing import List
 
 
-def shift(l: List[int], num: int) -> List[int]:
-
-    return l
+def shift(array: List[int], num: int) -> List[int]:
+    """function to perform a circular shift of a list to the left by a given number of elements."""
+    if num > len(array):
+        raise ValueError
+    for _ in range(num):
+        array.append(array.pop(0))
+    return array
