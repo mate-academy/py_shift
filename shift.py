e6d16d6c1014993dbf72d2c0ff6a1d3a75d2d19b
alexander <earthhuman41@gmail.com>
2019-11-24 19:24:50 +0200

    implemented shift

--- shift.py
+++ shift.py
@@ -1,6 +1,21 @@
+"""
+shift
+"""
 from typing import List
 
+def shift(numbers: List[int], num: int) -> List[int]:
+    """
+    :param numbers: [int] list of numbers
+    :param num: int times to shift
+    :return: [int] new list
+    """
+    if num > len(numbers):
+        raise ValueError
 
-def shift(l: List[int], num: int) -> List[int]:
+    new_numbers = numbers.copy()
 
-    return l
+    for _ in range(num):
+        shifted_element = new_numbers.pop(0)
+        new_numbers.append(shifted_element)
+
+    return new_numbers
