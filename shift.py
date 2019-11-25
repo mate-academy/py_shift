17a68328fc95165f1256992616abb97fc04a7fbd
Platoniuks <platon@alterpost.org>
2019-11-25 18:57:05 +0200

    resolved

--- shift.py
+++ shift.py
@@ -1,6 +1,17 @@
+"""The function to perform a circular shift of a list to the left by a given
+number of elements.
+"""
+
 from typing import List
 
 
-def shift(l: List[int], num: int) -> List[int]:
-
-    return l
+def shift(arr: List[int], num: int) -> List[int]:
+    """General func"""
+    if not num:
+        return arr
+    if num > len(arr):
+        raise ValueError
+    for _i in range(num):
+        value = arr.pop(0)
+        arr.append(value)
+    return arr
