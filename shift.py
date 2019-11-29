d82c1b99d57dcabdc97cfdab3c16ebd57bd597c8
koreyo <KoretsValentyn@gmail.com>
2019-11-30 00:52:50 +0200

    done

--- shift.py
+++ shift.py
@@ -1,6 +1,13 @@
+"""shift for many position"""
 from typing import List
 
 
-def shift(l: List[int], num: int) -> List[int]:
+def shift(item_list: List[int], num: int) -> List[int]:
+    """something to do"""
+    if num > len(item_list):
+        raise ValueError
 
-    return l
+    left = [i for i in item_list if item_list.index(i) < num]
+    right = [i for i in item_list if item_list.index(i) > num - 1]
+    right.extend(left)
+    return right
