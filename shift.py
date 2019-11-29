a269710f6e78bce3187c0c4aaaf7bc2bc34e7c40
zaikovlad95@gmail.com <Makepeacenotwar1995>
2019-11-29 17:57:21 +0200

    Create a func to shift a list to the left by a given number of elements.

--- shift.py
+++ shift.py
@@ -1,6 +1,12 @@
+"""Shift module."""
 from typing import List
 
 
-def shift(l: List[int], num: int) -> List[int]:
-
-    return l
+def shift(array: List[int], num: int) -> List[int]:
+    """shifts a list to the left by a given number of elements."""
+    if num > len(array):
+        raise ValueError
+    if array:
+        num = num % len(array)
+        return array[num:] + array[:num]
+    return []
